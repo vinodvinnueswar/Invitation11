8c3e7f5b074b166db44be2af0c43e8312afb3aba
Sasispecious <sasispecious@gmail.com>
2025-06-14 18:26:15 -0700

    stop in landscape

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import HomePage from './pages/HomePage'
-import Orientation from './helpers/Orientation'
 import './App.css'
 
 const App = () => {
   return (
     <div>
       <HomePage/>
-      <Orientation/>
     </div>
   )
 }
