21304761153cdf26a95ce40e8fab1522197dbabf
Sasispecious <sasispecious@gmail.com>
2025-06-30 13:21:58 -0700

    Adding Social Share

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+
+
+
 import facebook from '../images/Social_network/Facebook.png'
 import Instagram from '../images/Social_network/Instagram.png'
 import whatsapp from '../images/Social_network/Whatsapp1.png'
+import Share from './Share'
 
 const Contact = () => {
   return (
@@ -10,16 +14,20 @@ const Contact = () => {
             <h3>Thank You for Visting</h3>
         </div>
         <h3>Social Share</h3>
+        
+     <Share/>
+
+
    <div className="network">
-   <div className="facebook">
-        <img src={facebook} alt="" />
+   {/* <div className="facebook">
+       <img src={facebook} alt="" />
     </div>
     <div className="instagram">
-        <img src={Instagram} alt="" />
+    <img src={Instagram} alt="" />
     </div>
     <div className="whatsapp">
-        <img src={whatsapp} alt="" />
-    </div>
+    <img src={whatsapp} alt="" />
+    </div> */}
    </div>
     <div className="details">
         <img src="" alt="" />
